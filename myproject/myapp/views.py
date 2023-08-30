70b9afd3d38289dadac57acecd5ccbd6b64fda24
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-08-30 14:53:56 +0300

    logger mixin

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -3,18 +3,20 @@ from django.views.generic import TemplateView
 
 logger = logging.getLogger(__name__)
 
-def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
-    else:
-        ip = request.META.get('REMOTE_ADDR')
-    return ip
 
 class LoggerMixin:
     def get(self, request, *args, **kwargs):
-        logger.info(f"{request.path} accessed from {get_client_ip(request)}")
+        logger.info(f"{request.path} accessed from {self.get_client_ip(request)}")
         return super().get(self, request, *args, **kwargs)
+
+    @staticmethod
+    def get_client_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
         
 class IndexView(LoggerMixin, TemplateView):
     template_name = 'myapp/index.html'
