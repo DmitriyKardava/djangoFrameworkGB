dfa8dc999cd19af03bc0fa84b310596da51166b0
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-08-30 14:03:11 +0300

    lesson1

---
+++ myproject/myapp/views.py
@@ -0,0 +1,23 @@
+import logging
+from django.views.generic import TemplateView
+
+logger = logging.getLogger(__name__)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+class LoggedTemplateView(TemplateView):
+    def get(self, request, *args, **kwargs):
+        logger.info(f"{request.path} accessed from {get_client_ip(request)}")
+        return super().get(self, request, *args, **kwargs)
+        
+class IndexView(LoggedTemplateView):
+    template_name = 'myapp/index.html'
+    
+class AboutView(LoggedTemplateView):
+    template_name = 'myapp/about.html'
