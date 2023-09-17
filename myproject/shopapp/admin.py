697df0d9053c7fad83f5a215f452a2f941aeae44
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-09-17 17:24:53 +0300

    lesson5

--- myproject/shopapp/admin.py
+++ myproject/shopapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from shopapp.models import Client, Product, Order
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    list_filter = ['added_at', 'price']
+    search_fields = ['desc']
+    search_help_text = 'Поиск по полю Описание продукта (description)'
+class ItemsInline(admin.TabularInline):
+    model = Order.product.through
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [
+        ItemsInline,
+    ]
+
+admin.site.register(Client)
+admin.site.register(Product, ProductAdmin)
+
